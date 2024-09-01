998a11e45598cff1a1617fc00974f748d512e1f4
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-01 11:16:18 +0300

    example01 delegate demo

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace Demo
 {
+    //step 0 :declare Delegate
+    public delegate int StringFuncDelegate(string Str);
+
+    //will be translated to New Class [Delegate]
+    // Any Reference From This Delegate Can Refer To 
+    // Function Or More [Pointer To Function]
+    // This Function May be Static Function [Class Member Function]
+    // This Function May be Non Static Function [Object Member Function]
+    // But These Functions Must be with the Same Signature of The Delegate 
+    // Regardless Function Name & Access Modifier
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate Example 1
+            ///Delegate Is Class
+            ///Delegate is c# language feature 
+            ///has 2 usages
+            ///1.Pointer to Function (functinal programming)
+            ///2.Even driven programming
+            ///
+
+           int x= StringFunction.GetCountOfUpperCharacters("MarWa");
+           //int y = StringFunction.GetCountOfUpperCharacters;//invalid
+            
+
+
+            //Step 01: Declare Reference From Delegate
+            StringFuncDelegate stringFuncDelegate;
+            //Step 02: Initialize Reference From Delegate[Pointer To Function]
+            stringFuncDelegate = new StringFuncDelegate(StringFunction.GetCountOfUpperCharacters);
+            //stringFuncDelegate = StringFunction.GetCountOfUpperCharacters; //syntax sugar
+            //Step 03: Use Delegate Reference [Call Method] 
+            int y =stringFuncDelegate.Invoke("MatUPOPOj");
+            Console.WriteLine(y);
+
+
+            stringFuncDelegate+= new StringFuncDelegate(StringFunction.GetCountOfLowerCharacters);
+
+
+
+            #endregion
         }
     }
 }
---
+++ Demo/StringFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class StringFunction
+    {
+        public static int GetCountOfUpperCharacters(string name)
+        {
+            int Count = 0;
+            if (name is not null)
+                for (int i = 0; i < name.Length; i++)
+                    if (char.IsUpper(name[i]))
+                        Count++;
+            return Count;
+
+        }       
+        public static int GetCountOfLowerCharacters(string name)
+        {
+            int Count = 0;
+            if (name is not null)
+                for (int i = 0; i < name.Length; i++)
+                    if (char.IsLower(name[i]))
+                        Count++;
+            return Count;
+
+        }
+    }
+}
